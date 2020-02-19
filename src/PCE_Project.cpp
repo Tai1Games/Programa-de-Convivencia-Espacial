8bc978b3002c437075697d946497663ea6256ca9
Esteban <estrestrepo+github@gmail.com>
2020-02-19 16:49:57 +0100

    TTF implemented

--- src/PCE_Project.cpp
+++ src/PCE_Project.cpp
@@ -1,9 +1,11 @@
 #include "SDL.h"
 #include "box2d.h"
+#include "SDL_ttf.h"
 
 int main(int argc, char* argv[]) {
 	SDL_Rect a;
-	b2Vec2 gravity(0, -9.8);
+	b2Vec2 gravity(0, 0);
 	b2World world(gravity);
+	TTF_Font* font = 0;
 	return 0;
 }
