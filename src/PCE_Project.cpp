189a18c3ddd3e839259d69c2dc89ee835989c167
Esteban <estrestrepo+github@gmail.com>
2020-02-19 16:54:29 +0100

    Mixer Implemented

--- src/PCE_Project.cpp
+++ src/PCE_Project.cpp
@@ -1,11 +1,13 @@
 #include "SDL.h"
 #include "box2d.h"
 #include "SDL_ttf.h"
+#include "SDL_mixer.h"
 
 int main(int argc, char* argv[]) {
 	SDL_Rect a;
 	b2Vec2 gravity(0, 0);
 	b2World world(gravity);
 	TTF_Font* font = 0;
+	SDL_AudioStatus* status = 0;
 	return 0;
 }
