4ca215ed24ac8dff0f5443773c9cb38a0e9dc98b
Adrian Alvarez <adralv01@ucm.es>
2020-02-25 11:40:27 +0100

    Ventana creada

--- src/PCE_Project.cpp
+++ src/PCE_Project.cpp
@@ -3,7 +3,21 @@
 #include "SDL_ttf.h"
 #include "SDL_mixer.h"
 
+//puede meterse en el gameManager o un wrapper de ventana con mas funcionalidades
+// crea ventana y renderer
+void createWindow() {
+
+}
+
 int main(int argc, char* argv[]) {
-	SDL_Rect a;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("PCE",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		600,400,
+		SDL_WINDOW_SHOWN);
+
+	//descomentar para depurar
+	//while (true) {};
 	return 0;
 }
