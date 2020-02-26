faf03b4f421d721397efdbd0bc3b1ab5c9ab180e
jorgmo02 <jorgemomartin@gmail.com>
2020-02-26 17:44:40 +0100

    clase PlayState

---
+++ src/PlayState.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <list>
+class Entity; // sustituir
+
+using namespace std;
+
+// esta clase es equivalente a un nivel del juego
+class PlayState
+{
+public:
+
+private:
+	list<Entity*> stage;
+	//iterador que apunte al mapa
+
+};
+
