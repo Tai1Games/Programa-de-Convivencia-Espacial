53e52441d3dbaf66e213dea3f8d6b24ef98d7172
Adrian Alvarez <adralv01@ucm.es>
2020-02-26 16:38:11 +0100

    Implementación de la liibrería
    Y creación de un archivo de prueba

--- src/PCE_Project.cpp
+++ src/PCE_Project.cpp
@@ -1,4 +1,9 @@
 #include "SDL_Game.h"
+#include "json/single_include/nlohmann/json.hpp"
+#include <fstream>
+#include <iomanip>
+
+using json = nlohmann::json;
 
 //puede meterse en el gameManager o un wrapper de ventana con mas funcionalidades
 // crea ventana y renderer
@@ -7,6 +12,15 @@ void createWindow() {
 }
 
 int main(int argc, char* argv[]) {
+	json windowConfig;
+	windowConfig["width"] = 1280;
+	windowConfig["heigth"] = 720;
+	windowConfig["name"] = "El loco juego de Tai1 Games";
+
+	std::ofstream out("windowConfig.json");
+	out << std::setw(4) << windowConfig << std::endl;
+	out.close();
+
 	SDL_Game PCE = SDL_Game();
 
 
