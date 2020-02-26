37b7b496e0d72149fcca990faa2a1179df5e78c1
Esteban <estrestrepo+github@gmail.com>
2020-02-26 16:55:15 +0100

    GameManager instance done
    [Delivers #171442228]

--- src/SDL_Game.cpp
+++ src/SDL_Game.cpp
@@ -1,13 +1,31 @@
 #include "SDL_Game.h"
 
+unique_ptr<SDL_Game> SDL_Game::instance_;
+
 SDL_Game::SDL_Game() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	window_ = SDL_CreateWindow(WINDOW_NAME.c_str(),SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,WINDOW_WIDTH, WINDOW_HEIGHT,SDL_WINDOW_SHOWN);
 	renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
+
+	initializeResources();
 }
 
 SDL_Game::~SDL_Game() {
+	closeResources();
+
+	SDL_DestroyRenderer(renderer_);
+	renderer_ = nullptr;
+
+	SDL_DestroyWindow(window_);
 	window_ =  nullptr;
-	renderer_ =  nullptr;
+	
 	SDL_Quit();
+}
+
+void SDL_Game::initializeResources() {
+
+}
+
+void SDL_Game::closeResources() {
+
 }
