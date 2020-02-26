71cadc2940282c55e818c95bdb0158c050dd01ad
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2020-02-26 17:01:44 +0100

    Clase Componente y ComponentType
    La componente es practicamente la misma que en tpv 2 pero tiene los metodos init, update, draw virtuales puros, evitando crear componentes por si solos.
    
    Se ha renombrado el ecs a ComponentType para que sea mas entendible

---
+++ src/Component.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <SDL.h>
+
+#include "ComponentType.h"
+#include "SDL_Game.h"
+
+class Entity;
+
+
+class Component {
+protected:
+	Entity* entity_;
+	SDL_Game* game_;
+	ComponentType::CmpId id_;
+
+	//Las clases que hereden de este tendrán un estado propio
+public:
+	Component(ComponentType::CmpId id);
+	virtual ~Component();
+
+	void setEntity(Entity* entity) {
+		entity_ = entity;
+	}
+
+	void setGame(SDL_Game* game) {
+		game_ = game;
+		
+	}
+
+	void setId(ComponentType::CmpId id) {
+		id_ = id;
+	}
+
+	ComponentType::CmpId getId() const {
+		return id_;
+	}
+
+	//Metodos que implementan los hijos y los dotan de funcionalidad
+	virtual void init() = 0;
+	virtual void update() = 0;
+	virtual void draw() = 0;
+
+};
+
+
