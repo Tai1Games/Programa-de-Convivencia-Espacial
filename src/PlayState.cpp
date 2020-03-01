090849232c53c9b10f06affd2e5cc1e2e60c5dd0
Will7461 <43720307+Will7461@users.noreply.github.com>
2020-03-01 19:32:30 +0100

    Tinky que cae

--- src/PlayState.cpp
+++ src/PlayState.cpp
@@ -18,13 +18,36 @@ void PlayState::init() {
 	entityManager_ = new EntityManager();
 	physicsWorld_ = new b2World(b2Vec2(0, 0));
 
-	Entity* tinky = entityManager_->addEntity();
-	tinky->addComponent<Transform>(Vector2D(200.0,200.0),0.0,100.0,100.0);
-	tinky->addComponent<Viewer>();
+	b2BodyDef wallBodyDef;
+	b2BodyDef tinkyBodyDef;
+	tinkyBodyDef.type = b2_dynamicBody;
+	tinkyBodyDef.position.Set(200, 200);
+	wallBodyDef.position.Set(0, -500);
 
+	b2Body* wallBody = physicsWorld_->CreateBody(&wallBodyDef);
+	b2Body* tinkyBody = physicsWorld_->CreateBody(&tinkyBodyDef);
+
+	b2PolygonShape boxShape;
+	b2PolygonShape tinkyShape;
+	boxShape.SetAsBox(1000,5);
+	tinkyShape.SetAsBox(10, 10);
+	
+	wallBody->CreateFixture(&boxShape,0);
+	tinkyBody->CreateFixture(&tinkyShape, 0);
+	
+	Entity* tinky = entityManager_->addEntity();
+	Entity* ground = entityManager_->addEntity();
+	Transform* tr = tinky->addComponent<Transform>(tinkyBody);
+	Transform* trG = ground->addComponent<Transform>(wallBody);
+	tinky->addComponent<Viewer>();
+	ground->addComponent<Viewer>();
+	//tr->getBody()->ApplyForce(b2Vec2 (0, -200), b2Vec2(0, 0), true);
+	tr->getBody()->ApplyLinearImpulse(b2Vec2(0, -100), b2Vec2(0, 0),true);
 }
+
 void PlayState::update() {
 	entityManager_->update();
+	physicsWorld_->Step(1.0f / 60.0f, 6, 2);
 	//también debería actualizar la lógica de modo de juego
 	//spawners de monedas, carga de objetivos...
 }
--- src/Transform.h
+++ src/Transform.h
@@ -2,6 +2,7 @@
 #include "Component.h"
 #include "Vector2D.h"
 #include "checkML.h"
+#include "Box2D/include/box2d/box2d.h"
 
 
 class Transform : public Component
@@ -10,21 +11,24 @@ public:
 	Transform(Vector2D pos, double rot, double w, double h) : Component(ComponentType::Transform) {
 		position_ = pos; rotation_ = rot; width_ = w, heigth_ = h;
 	};
+	Transform(b2Body* b) : Component(ComponentType::Transform){
+		body_ = b; position_ = Vector2D(0,0); rotation_ = 0; width_ = 200, heigth_ = 200;
+	}
 	virtual ~Transform() {};
 
 	//posición
-	const Vector2D& getPos() const { return position_; }
-	double getPosX() { return position_.getX(); }
-	double getPosY() { return position_.getY(); }
+	const Vector2D& getPos() const { return Vector2D((double)body_->GetPosition().x,-(double)body_->GetPosition().y); }
+	double getPosX() { return (double)body_->GetPosition().x; }
+	double getPosY() { return -(double)body_->GetPosition().y; }
 
-	void setPos(Vector2D vec) { position_ = vec; }
-	void setPos(double x, double y) { position_.set(x, y); }
-	void setPosX(double x) { position_.setX(x); }
-	void setPosY(double y) { position_.setY(y); }
+	void setPos(Vector2D vec) { body_->SetTransform(b2Vec2(vec.getX(),-vec.getY()),body_->GetAngle()); }
+	void setPos(double x, double y) { body_->SetTransform(b2Vec2(x, -y), body_->GetAngle()); }
+	void setPosX(double x) { body_->SetTransform(b2Vec2(x,body_->GetPosition().y),body_->GetAngle()); }
+	void setPosY(double y) {  body_->SetTransform(b2Vec2(body_->GetPosition().x,-y),body_->GetAngle());  }
 
 	//rotación
-	double getRot() { return rotation_; }
-	void setRot(int rot) { rotation_ = rot; }
+	double getRot() { return (double)body_->GetAngle(); }
+	void setRot(int rot) { body_->SetTransform(b2Vec2(body_->GetPosition().x, body_->GetPosition().y), rot); }
 
 	//width, heigth
 	double getW() { return width_; }
@@ -33,12 +37,14 @@ public:
 	void setW(double w) { width_ = w; }
 	void setH(double h) { heigth_ = h; }
 
+	b2Body* getBody() { return body_; }
+
 	//destRect
 	SDL_Rect getDestRect() const {
 		SDL_Rect r;
 
-		r.x = position_.getX();
-		r.y = position_.getY();
+		r.x = body_->GetPosition().x;
+		r.y = -body_->GetPosition().y;
 		r.w = width_;
 		r.h = heigth_;
 
@@ -46,6 +52,7 @@ public:
 	}
 
 private:
+	b2Body* body_;
 	Vector2D position_;
 	double rotation_;
 	double width_;
