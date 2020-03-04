d72804b6f119a16b3b80ee07a8ccecac59197201
Andres <47799692+AndresRuizB@users.noreply.github.com>
2020-03-04 17:17:22 +0100

    añadido tinky supersonico

--- src/SourceFiles/PlayState.cpp
+++ src/SourceFiles/PlayState.cpp
@@ -16,19 +16,25 @@ void PlayState::init() {
 	//aqui se crean todas las entidades necesarias
 	//se podría JSONizar para evitar compilar
 	entityManager_ = new EntityManager();
-	physicsWorld_ = new b2World(b2Vec2(0, 9.8));
+	physicsWorld_ = new b2World(b2Vec2(0, 0));
 	
 	Entity* tinky = entityManager_->addEntity();
 	Entity* ground = entityManager_->addEntity();
 	Entity* rock = entityManager_->addEntity();
+	Entity* spaceJunk = entityManager_->addEntity();
+
 	Collider* collTinky = tinky->addComponent<Collider>(physicsWorld_, b2_dynamicBody, 150, 80, 50, 50, 50, 0, false);
 	Collider* collSuelo = ground->addComponent<Collider>(physicsWorld_, b2_staticBody, 0, 500, 1000, 10, 50, 0, false);
 	Collider* collRock = rock->addComponent<Collider>(physicsWorld_, b2_dynamicBody, 500, 80, 20, 20, 10, 0, false);
+	Collider* collJunk = spaceJunk->addComponent<Collider>(physicsWorld_, b2_dynamicBody, 300, 60, 60, 30, 0.5, 0, false);
 	//Transform* tr = tinky->addComponent<Transform>(tinkyBody);
 	//Transform* trG = ground->addComponent<Transform>(wallBody);
 	tinky->addComponent<Viewer>(Resources::Tinky);		//  <-- se puede poner un sprite con esta constructora, pero por defecto sale un tinky.
 	ground->addComponent<Viewer>();						//  también se puede poner un SDL_Rect para el clip (después de la textura)
 	rock->addComponent < Viewer >();
+	spaceJunk->addComponent<Viewer>();
+
+	collJunk->applyLinearImpulse(b2Vec2(0,100000), b2Vec2(0,0), true);
 	//tr->getBody()->ApplyForce(b2Vec2 (0, -200), b2Vec2(0, 0), true);
 	//tr->getBody()->ApplyLinearImpulse(b2Vec2(0, -100), b2Vec2(0, 0),true);
 }
