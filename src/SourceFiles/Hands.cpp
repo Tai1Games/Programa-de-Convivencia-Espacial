4f5dc144e2eb84ecdeaa0b6acbecbc392dc2322f
Pj <pjurad01@ucm.es>
2020-03-13 14:46:18 +0100

    Update de las manos (intentando determinar el cáculo de la posición)

--- src/SourceFiles/Hands.cpp
+++ src/SourceFiles/Hands.cpp
@@ -24,18 +24,24 @@ Hands::Hands(int playerID, int textureId, SDL_Rect clip) :
 void Hands::init()
 {
 	collider_ = GETCMP1_(Collider);
-	/*if (tex_ == nullptr) {
+	if (tex_ == nullptr) {
 		tex_ = game_->getTexturesMngr()->getTexture(Resources::Tinky);
-	}*/
+	}
 }
 
 void Hands::draw() const
 {
-	//tex_->render(collider_->getRect(), collider_->getAngle(), clip_);
+	SDL_Rect colRec = collider_->getRectRender();
+	SDL_Rect destRect;
+	destRect.w = colRec.w / 2;
+	destRect.h = colRec.h / 2;
+	destRect.x = colRec.x + (colRec.w + colRec.w/5);
+	destRect.y = colRec.y + (colRec.h / 2) - (destRect.h/2);
+	tex_->render(destRect, collider_->getAngle()/*, clip_*/);
 }
 
 void Hands::update() const {
-
+	//pos_.x = collider_->getPos().x + collider_->getPos
 }
 
 void Hands::setWeapon(int weaponColumn)
