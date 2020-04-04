f2913faf296c030a091db3df4148c4d4b2834717
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2020-04-04 18:22:02 +0200

    Listo para meter en master
    Se ha mergeado con master y se han solucionado los problemas tipicos

--- src/SourceFiles/WeaponFactory.h
+++ src/SourceFiles/WeaponFactory.h
@@ -13,9 +13,6 @@ public:
 	WeaponFactory() {};
 
 	~WeaponFactory() {};
-	//Crea un mando de la tele (espec�fico para el modo Pelea Por El Mando)
-	Weapon* makeController(EntityManager* entityManager, b2World* physiscsWorld, b2Vec2 pos, b2Vec2 size); 
-
 	//Crea una chancla (Podemos pasarle los p�rametros que queramos segun las necesidades del objeto)
 	static void makeSlipper(EntityManager* entityManager, b2World* physicsWorld, b2Vec2 pos, b2Vec2 size);
 
