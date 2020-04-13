cb3b0eb5899c3dead503e98636c5fc61d3094f7f
AdriAlvarez00 <adralv01@ucm.es>
2020-04-13 17:00:48 +0200

    Otro cambio menor de constantes

--- src/SourceFiles/Fireball.cpp
+++ src/SourceFiles/Fireball.cpp
@@ -15,7 +15,7 @@ void Fireball::setActive(bool to,b2Vec2 pos)
 	entity_->setActive(to);
 	col_->getFixture(0)->GetBody()->SetTransform(pos, 0);
 	if (to) {
-		col_->createCircularFixture(CONST(double, "COIN_BASE_SIZE"), CONST(double, "COIN_DENSITY"), CONST(double, "COIN_FRICTION"), CONST(double, "COIN_RESTITUTION"), Collider::CollisionLayer::NormalObject, false);
+		col_->createCircularFixture(CONST(double, "FIREBALL_RADIUS"), CONST(double, "FIREBALL_DENSITY"), CONST(double, "FIREBALL_FRICTION"), CONST(double, "FIREBALL_RESTITUTION"), Collider::CollisionLayer::NormalObject, false);
 	}
 	else if (col_->getNumFixtures() > 1) col_->destroyFixture(1);
 
