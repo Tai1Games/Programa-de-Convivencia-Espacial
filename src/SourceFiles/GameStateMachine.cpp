65a09e481c62a2b389e22813dc2a3692b3ebbf27
Esteban <35503563+EstebenR@users.noreply.github.com>
2020-04-18 14:30:52 +0200

    GameStateMachine ya admite varios modos

--- src/SourceFiles/GameStateMachine.cpp
+++ src/SourceFiles/GameStateMachine.cpp
@@ -22,11 +22,11 @@ GameStateMachine::~GameStateMachine() {
 void GameStateMachine::setPauseOwner(int ownerID)
 {
 	changeToState(States::pause);
-	if(PauseState* pause = static_cast<PauseState*>(states_[States::pause]))
-	pause->setOwner(ownerID);
+	if (PauseState* pause = static_cast<PauseState*>(states_[States::pause]))
+		pause->setOwner(ownerID);
 }
 
-void GameStateMachine::changeToState(int state) {
+void GameStateMachine::changeToState(int state, int numberOfPlayers, int gameMode, string tileMap) {
 	if (state != currentState_ && state < States::NUMBER_OF_STATES) {
 		if (states_[state] == nullptr) {
 			//create state
@@ -36,11 +36,28 @@ void GameStateMachine::changeToState(int state) {
 				//deleteState(currentState_);
 				break;
 			case States::play:
-				//deleteState(currentState_);
-
-				states_[state] = new PlayState(new CapitalismGameMode(4), "BoilerRoom"); //LivingRoom, BoilerRoom
-
+			{
+				if (gameMode < NUMBER_OF_GAMEMODES) {
+					switch (gameMode) {
+					case (GamemodeID::Capitalism):
+						states_[state] = new PlayState(new CapitalismGameMode(numberOfPlayers), tileMap);
+						break;
+					case (GamemodeID::Controller):
+						states_[state] = new PlayState(new ControllerGameMode(numberOfPlayers), tileMap);
+						break;
+					case (GamemodeID::Stocks):
+						states_[state] = new PlayState(new StocksGameMode(numberOfPlayers), tileMap);
+						break;
+					case (GamemodeID::WiFight):
+						states_[state] = new PlayState(new WiFightGameMode(numberOfPlayers), tileMap);
+						break;
+						/*case (GamemodeID::Timed):
+							states_[state] = new PlayState(new WiFightGameMode(numberOfPlayers), tileMap);
+							break;*/
+					}
+				}
 				break;
+			}
 			case States::pause:
 				//if (states_[state] != nullptr)	delete states_[state];
 				states_[state] = new PauseState();
