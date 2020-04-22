8907944dad1fd8a9c4c8f9b6d5b74edc360fec04
Esteban <35503563+EstebenR@users.noreply.github.com>
2020-04-22 22:20:25 +0200

    Some pre-destroy cleanup

--- src/SourceFiles/MeleeWeapon.h
+++ src/SourceFiles/MeleeWeapon.h
@@ -5,8 +5,6 @@ class MeleeWeapon :
 	public ActionableWeapon
 {
 protected:
-	std::vector<EnemyData>playersInsideRange_;
-	int index = 2;
 
 	int damage_ = 0;
 public:
@@ -14,14 +12,15 @@ public:
 	MeleeWeapon(ComponentType::CmpId compType, WeaponID wId, int dmg, int impactDmg, int cooldownFrames);
 	virtual ~MeleeWeapon() {};
 
-	virtual void init() override;
+	//virtual void init() override;
 	virtual void action() override;
 	//Alterar el viewer
 
 	virtual void PickObjectBy(Hands* playerHands) override;
+	virtual void onCollisionEnter(Collision* c) override;
 	virtual void UnPickObject() override;
 
-	virtual void detectPlayer(Entity* playerDetected, int id) override;
-	virtual void loseContactPlayer(Entity* playerDetected, int id) override;
+	//virtual void detectPlayer(Entity* playerDetected, int id) override;
+	//virtual void loseContactPlayer(Entity* playerDetected, int id) override;
 };
 
