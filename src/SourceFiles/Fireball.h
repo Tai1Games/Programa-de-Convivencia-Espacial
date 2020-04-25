93aa8c4bc9f405e391ae46d63e179b97ae77e302
AdriAlvarez00 <48672128+AdriAlvarez00@users.noreply.github.com>
2020-04-25 17:14:00 +0200

    El fuego quita monedas (#103)

--- src/SourceFiles/Fireball.h
+++ src/SourceFiles/Fireball.h
@@ -10,8 +10,9 @@ private:
 	const static int damage_ = 1000; //daño que hace al impactar con el jugador
 	Viewer* vw_ = nullptr;
 	Collider* col_ = nullptr;
+	int coinDMG;
 public:
-	Fireball() :Component(ComponentType::Fireball) {}
+	Fireball() :Component(ComponentType::Fireball),coinDMG(CONST(int,"FIREBALL_COIN_DMG")) {}
 	~Fireball() {};
 	virtual void init() override;
 	void setActive(bool to, b2Vec2 pos);
