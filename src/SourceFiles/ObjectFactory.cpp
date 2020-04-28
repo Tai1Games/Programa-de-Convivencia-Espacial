c1cd6fa600d85b99b4c4e033100747f7c0e73129
Will7461 <43720307+Will7461@users.noreply.github.com>
2020-04-28 22:42:57 +0200

    BugFix: Vuelve el rango para recoger armas + El mando vuelve a colisionar con el mapa (#119)
    * Las armas vuelven a tener rango para cogerlas
    
    * El mando ya no se sale del mapa
    
    * Fixeado fixture business en los meleeWeapons
    
    Co-authored-by: Esteban <35503563+EstebenR@users.noreply.github.com>

--- src/SourceFiles/ObjectFactory.cpp
+++ src/SourceFiles/ObjectFactory.cpp
@@ -94,7 +94,7 @@ Entity* ObjectFactory::makeTomato(Entity* e, EntityManager* entityManager, b2Wor
 Weapon* ObjectFactory::makeController(EntityManager* entityManager, b2World* physicsWorld, b2Vec2 pos, b2Vec2 size) {
 	Entity* e = entityManager->addEntity();
 	Collider* aux = e->addComponent<Collider>(physicsWorld, b2_dynamicBody, pos.x, pos.y, size.x, size.y, CONST(double, "CONTROLLER_DENSITY"), CONST(double, "CONTROLLER_FRICTION"),
-		CONST(double, "CONTROLLER_RESTITUTION"), CONST(double, "CONTROLLER_LINEAR_DRAG"), CONST(double, "CONTROLLER_ANGULAR_DRAG"), Collider::CollisionLayer::Trigger, false);
+		CONST(double, "CONTROLLER_RESTITUTION"), CONST(double, "CONTROLLER_LINEAR_DRAG"), CONST(double, "CONTROLLER_ANGULAR_DRAG"), Collider::CollisionLayer::NormalObject, false);
 	e->addComponent<Viewer>(Resources::Remote);
 	Weapon* controller = e->addComponent<Weapon>(WeaponID::Mando, CONST(int, "CONTROLLER_IMPACT_DAMAGE"));
 	e->addComponent<ColliderViewer>();
