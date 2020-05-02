7daea01f6544ecf39143cb9bb9ae6bdbd2f67aba
JuanmaSalva <jsalva@ucm.es>
2020-05-02 18:58:03 +0200

    balas TOTALMENTE funcionales
    creo y espero

--- src/SourceFiles/Bullet.cpp
+++ src/SourceFiles/Bullet.cpp
@@ -2,8 +2,21 @@
 
 void Bullet::init()
 {
+	winWidth_ = CONST(int, "WINDOW_WIDTH");
+	winHeight_ = CONST(int, "WINDOW_HEIGHT");
 	col_ = GETCMP1_(Collider);
 	viewer_ = GETCMP1_(Viewer);
+}
+
+void Bullet::update()
+{
+	if (col_->getPos().x < 0 || col_->getPos().y > winWidth_ || //se elimina si se sale de la pantalla
+		col_->getPos().y < 0 || col_->getPos().y > winHeight_) needToDelete = true;
+
+	if (needToDelete) {
+		setActive(false);
+		needToDelete = false;
+	}
 }
 
 void Bullet::setActive(bool a, b2Vec2 pos, b2Vec2 size, b2Vec2 vel, int texture, int damage, int player)
@@ -13,7 +26,7 @@ void Bullet::setActive(bool a, b2Vec2 pos, b2Vec2 size, b2Vec2 vel, int texture,
 	col_->getBody()->SetEnabled(a);
 
 	if (a) { //hacer movidas
-		col_->getBody()->SetTransform(pos,0);
+		col_->getBody()->SetTransform(pos, 0);
 		col_->applyLinearImpulse(vel, { 0,0 });
 		//SIZE------------
 		viewer_->setTexture(texture);
@@ -26,7 +39,19 @@ void Bullet::onCollisionEnter(Collision* c)
 {
 	PlayerData* pData = c->entity->getComponent<PlayerData>(ComponentType::PlayerData);
 	if (pData != nullptr && pData->getPlayerNumber() != player_) { //solo colisiona con jugadores que no sea el que lo ha lanzado
-		std::cout << "aaaaaaaaaaa" << endl;
-
+		Health* pHealth_ = c->entity->getComponent<Health>(ComponentType::Health);
+		if (pHealth_ != nullptr) {
+			if (!pHealth_->subtractLife(damage_)) {
+				pHealth_->playerDead(c->collisionHandler);
+				needToDelete = true;
+			}
+		}
+		else {
+			Wallet* wallet = GETCMP2(c->entity, Wallet);
+			if (wallet != nullptr) {
+				c->collisionHandler->addCoinDrop(std::make_tuple(wallet, pData, CONST(int, "BANANA_BULLET_COIN_DMG")));
+				needToDelete = true;
+			}
+		}
 	}
 }
--- src/SourceFiles/Bullet.h
+++ src/SourceFiles/Bullet.h
@@ -4,20 +4,24 @@
 #include "Viewer.h"
 #include "Collision.h"
 #include "PlayerData.h"
+#include "Health.h"
+#include "Wallet.h"
+#include "CollisionHandler.h"
 
 class Bullet: public Component
 {
 private:
 	Collider* col_;
 	Viewer* viewer_;
-	int player_;
-	int damage_ = 0;
+	int player_, damage_ = 0, winWidth_, winHeight_;
+	bool needToDelete = false;
 
 public:
 	Bullet():Component(ComponentType::Bullet){}
 	~Bullet(){}
 
 	virtual void init();
+	virtual void update() override;
 	virtual void setActive(bool a, b2Vec2 pos = { 0,0 }, b2Vec2 size = { 0,0 }, b2Vec2 vel = {0,0}, int texture = 0, int damage = 0, int player = -1);
 	virtual void onCollisionEnter(Collision* c);
 };
