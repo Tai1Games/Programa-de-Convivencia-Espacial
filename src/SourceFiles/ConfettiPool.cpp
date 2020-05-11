53874ef1e15f3bf9bcedef3ab5d4d0b45f00c83a
Jorge Moreno Martínez <jorgmo02@ucm.es>
2020-05-11 15:20:16 +0200

    Cambio nombre

--- src/SourceFiles/ConfettiPool.cpp
+++ src/SourceFiles/ConfettiPool.cpp
@@ -7,8 +7,8 @@ ConfettiPool::ConfettiPool() : confettiPool_([](Entity* e) {return e->isActive()
 void ConfettiPool::init(EntityManager* eMan, b2World* physicsWorld, GameMode* gM)
 {	
 	b2Vec2 confettiSize = b2Vec2(CONST(float, "CONFETTI_X"), CONST(float, "CONFETTI_Y"));
-	vector<Entity*> tomatoPool = confettiPool_.getPool();
-	for (Entity* e : tomatoPool) {
+	vector<Entity*> confettiPoolVector= confettiPool_.getPool();
+	for (Entity* e : confettiPoolVector) {
 		e = ObjectFactory::makeConfetti(e, eMan, physicsWorld, b2Vec2(0, 0), confettiSize, gM);
 		e->getComponent<ConfettiWeapon>(ComponentType::ConfettiWeapon)->setActive(false);
 	}
