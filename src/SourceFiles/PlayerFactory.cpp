530008071c1a3a09acc2e208d543664ff849d9b3
Will7461 <43720307+Will7461@users.noreply.github.com>
2020-05-15 19:29:40 +0200

    Buena excepcion en Master cracks

--- src/SourceFiles/PlayerFactory.cpp
+++ src/SourceFiles/PlayerFactory.cpp
@@ -10,7 +10,7 @@
 Entity* PlayerFactory::createBasePlayer(EntityManager* eMan, b2World* physicsWorld, int playerID, int playerTextureID, int posX, int posY, InputBinder* ib) {
 	Entity* p = eMan->addEntity();
 	Collider::CollisionLayer layer = (Collider::CollisionLayer)(Collider::CollisionLayer::Player1 * pow(2, playerID));
-	Collider* collP = p->addComponent<Collider>(physicsWorld, b2_dynamicBody, posX, posY, CONST(double, "PLAYER_HEIGTH"), CONST(double, "PLAYER_WIDTH"), CONST(double, "PLAYER_DENSITY"), CONST(double, "PLAYER_FRICTION"), CONST(double, "PLAYER_RESTITUTION"), CONST(double, "PLAYER_LINEAR_DRAG"), CONST(double, "PLAYER_ANGULAR_DRAG"), layer, false);
+	Collider* collP = p->addComponent<Collider>(physicsWorld, b2_dynamicBody, posX, posY, CONST(double, "PLAYER_WIDTH"), CONST(double, "PLAYER_HEIGHT"), CONST(double, "PLAYER_DENSITY"), CONST(double, "PLAYER_FRICTION"), CONST(double, "PLAYER_RESTITUTION"), CONST(double, "PLAYER_LINEAR_DRAG"), CONST(double, "PLAYER_ANGULAR_DRAG"), layer, false);
 	p->addComponent<PlayerData>(playerID,ib);
 	p->addComponent<AnimatedPlayer>(Resources::PlayerAnimSheet, 0);
 	p->addComponent<ColliderViewer>();
